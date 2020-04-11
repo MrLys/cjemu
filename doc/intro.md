f0d882d329faf85dcc44f27e17cca8105ea674b4
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2020-04-11 20:46:09 +0200

    Add template lein app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljemu
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cljemu/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljemu.core-test
+  (:require [clojure.test :refer :all]
+            [cljemu.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
